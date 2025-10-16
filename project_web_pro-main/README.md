fdbb61516d3089daeb279063de6c95457e7d0f71
GotTapanapong <138994787+GotTapanapong@users.noreply.github.com>
2025-10-16 13:26:33 +0700

    Delete project_web_pro-main directory

--- project_web_pro-main/README.md
+++
@@ -1,2 +0,0 @@
-# project_web_pro
-testttt
--- project_web_pro-main/public/js/point.js
+++
@@ -1,6 +0,0 @@
-// เปิด-ปิด Navbar เมื่อคลิกขีดสามขีด
-document.getElementById('menu-toggle').addEventListener('click', function() {
-    const navbarMenu = document.querySelector('.navbar-menu');
-    navbarMenu.classList.toggle('active');
-});
-
